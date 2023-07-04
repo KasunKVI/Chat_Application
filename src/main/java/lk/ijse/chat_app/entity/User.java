5142391fd4c1e6a097ff2410cd2285a0d365633d
KasunKVI <kkhettiarachchi10@gmail.com>
2023-07-04 16:12:09 +0530

    implement entity package

---
+++ src/main/java/lk/ijse/chat_app/entity/User.java
@@ -0,0 +1,19 @@
+package lk.ijse.chat_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class User {
+
+    private String user_name;
+    private  Integer userI_id;
+    private String address;
+
+}
